1b2c7bd65e38d63c30d025e62a0e45d3a0259ccb
Icen <105867625+icenspln@users.noreply.github.com>
2023-06-19 19:51:42 +0100

    front: navbar

---
+++ AnimalShelter/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+  return (
+    <nav className="px-2 py-5">
+      <div className="flex justify-between">
+        <h1 className="font-black text-xl">LOGO</h1>
+        <button className="bg-greenish text-white font-bold py-1 px-4">
+          Login
+        </button>
+      </div>
+    </nav>
+  );
+}
--- AnimalShelter/tailwind.config.js
+++ AnimalShelter/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenish: "#4EEA96"
+      }
+    },
   },
   plugins: [],
 }
