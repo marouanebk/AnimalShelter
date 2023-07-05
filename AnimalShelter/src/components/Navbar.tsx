e515785bb3c6723db8b7fb9f6cd72eb195339949
Icen <105867625+icenspln@users.noreply.github.com>
2023-07-05 12:19:10 +0100

    added user button

--- AnimalShelter/src/components/Navbar.tsx
+++ AnimalShelter/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { FaUser } from "react-icons/fa";
 
 export function Navbar() {
   const { pathname } = useLocation();
@@ -9,14 +10,11 @@ export function Navbar() {
   if (pathname == "/login" || pathname == "/signup") {
     check = false;
   }
-  const { currentUser, dispatch }: any = useContext(AuthContext)
+  const { currentUser, dispatch }: any = useContext(AuthContext);
 
   const handleLogout = () => {
-    console.log(currentUser);
     dispatch({ type: "LOGOUT" });
   };
-
-
 
   return (
     <nav className="container px-6 mx-auto">
@@ -25,12 +23,17 @@ export function Navbar() {
           <h1 className="font-black text-xl">Animal Shelter</h1>
         </Link>
         {check && currentUser ? (
-          <button
-            className="border-2 border-black bg-blueish font-bold py-1 px-4"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
+          <div className="flex gap-4 items-center justify-between">
+            <Link to={`/user/${currentUser.token}`}>
+              <FaUser className="text-2xl" />
+            </Link>
+            <button
+              className="border-2 border-black bg-blueish font-bold py-1 px-4"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
         ) : (
           check && (
             <Link to="/login">
