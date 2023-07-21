8c0ebb0b500d743466ca5973d4e4427e084b8312
Boukandoura Merouane <m_boukandoura@estin.dz>
2023-07-21 14:43:12 +0100

    fixed the favorite bug

--- backend/controllers/dashboard_controller.js
+++ backend/controllers/dashboard_controller.js
@@ -28,17 +28,28 @@ exports.getFavoritesByUser = async (req, res, next) => {
   const { userId } = req.params;
 
   try {
-    const favorites = await Favorite.find({ userId })
-      .populate({
-        path: 'adId',
-        populate: {
-          path: 'owner',
-          // model: 'User',
-        },
-      })
+    // Verify that userId is a valid ObjectId before querying the database
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: 'Invalid userId format' });
+    }
+
+    // Retrieve user's favorites without populating the ad details
+    const favorites = await Favorite.find({ userId }).exec();
+
+    // Retrieve the valid adIds from the favorites
+    const validAdIds = favorites.map((favorite) => favorite.adId);
+
+    // Find the ads with validAdIds and populate the 'owner' field
+    const validAds = await Ad.find({ _id: { $in: validAdIds } })
+      .populate('owner')
       .exec();
 
-    res.status(200).json({ favorites });
+    // Filter out any favorites with deleted ads
+    const validFavorites = favorites.filter((favorite) =>
+      validAdIds.includes(favorite.adId)
+    );
+
+    res.status(200).json({ favorites: validFavorites, validAds });
   } catch (error) {
     next(error);
   }
